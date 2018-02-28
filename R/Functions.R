fe12806f6875ea4a579f150de6923ef9ef7c0fd3
David <baraghoshi.d@gmail.com>
2018-02-28 13:47:28 -0700

    Update the package

--- R/Functions.R
+++ R/Functions.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(wooldridge)
 
+# 3. Generate an Exponential Distribution #
 exp_density <- function(size = 100, mean = 1){
   density <- data.frame(matrix(NA, nrow = size))
  density[,1] <- rexp(size, r = 1/mean)
@@ -10,6 +11,7 @@ exp_density <- function(size = 100, mean = 1){
 }
 
 
+# 4. A custom theme in ggplot #
 my_theme <- function(){
   theme(
     axis.title.x = element_blank()
@@ -22,13 +24,10 @@ my_theme <- function(){
   )
 }
 
-ggplot(exp_density(1000, 4), aes(x = matrix.NA..nrow...size.)) +
-  geom_density() +
-  my_theme()
 
 
-
-top_n <- function(mydata = alcohol, groupings = "status", n = 1){
+# 4. Get top groups by frequency #
+top_n <- function(mydata = data, groupings = " ", n = 1){
   library(tidyverse)
   x <-
   mydata %>%
@@ -41,7 +40,32 @@ top_n <- function(mydata = alcohol, groupings = "status", n = 1){
   return(x)
 }
 
+#5. #
+# Approximate Power Calculation #
+power_calc <- function(samples = 1000, alpha = 0.05, mu0 = 1, mu1 = 1){
+  ps <- rep(NA, 10000)
+  for(i in 1:10000){
+  x <- rnorm(samples, mu0)
+  tt <- t.test(x, mu = mu1, conf.level = 1-alpha)
+  ps[i]<-tt$p.value
+}
+return(mean(ps <= 0.05))
+}
 
+# Calcute the Maximun Likelihood Estimates for a Normally Dist. Dataset #
 
+normal_mle <- function(mydata = data){
+  mu <- mean(mydata)
+  delta  = mydata - mu
+  d2 = delta^2
+  sumd2 = sum(d2)
+  sigma2 = sumd2/length(mydata)
+  mle<-c(mu, sigma2)
+  return(mle)
+}
+
+# THird function #
+
+# Fourth Function #
 
 
